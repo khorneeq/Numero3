5af612a43578abac4abf8b67544dc7dac15e63f7
khorneeq <130909083+khorneeq@users.noreply.github.com>
2023-04-26 13:16:59 +0200

    Add files via upload
    numero 3 rozwiązane

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Numero_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 20;
+            var y = 25;
+
+            Console.WriteLine(Checker(x, y));
+        }
+
+        static bool Checker(int x, int y)
+        {
+            return x == 30 || y == 30 || (x + y) == 30;
+            
+        }
+    }
+}
